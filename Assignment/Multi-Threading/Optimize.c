6dec6099998ba333801d47596421ee3cff736e0c
Yunjong-Boo <boo0117@naver.com>
2022-02-24 13:29:08 +0900

    Optimize ver2

--- Assignment/Multi-Threading/Optimize.c
+++ Assignment/Multi-Threading/Optimize.c
@@ -6,33 +6,31 @@
 #include <pthread.h>
 #include <time.h>
 
-#define TILE_SIZE 256 	//Change according to L1 Cache Memory Size
+#define STANDARD_SIZE 256 	//Change according to L1 Cache Memory Size
 #define Nthread 12	//Change according to number of CPU core
 
 typedef struct _MatrixInfo{
 	int rows;
 	int columns;
-	int matrix_size;
 } MatrixInfo;
 
 typedef struct _ThreadParameter{
-	int Ntile;
+	int tid, num_tile;
 	int *A_Mat, *B_Mat, *res_Mat;
 	MatrixInfo res_Info;
 //A_Info, B_Info;
 } Parameter;
 
-void PrintMatrix(int *mat, MatrixInfo *info){
+void PrintMatrix(int **mat, MatrixInfo *info){
 	for(int i = 0; i < info->rows; i++){
 		for(int j = 0; j < info->columns; j++){
-			printf("%d ", mat[i*info->rows + j]);
+			printf("%d ", mat[i][j]);
 		}
 		printf("\n");
 	}
 }
 
-int *ReadMatrix(char *file_name, MatrixInfo *info){
-printf("In ReadMatrix\n");
+int **ReadMatrix(char *file_name, MatrixInfo *info){
 	/* Open & Checking file */
 	FILE *fp = fopen(file_name, "r");
 	if(fp == NULL){
@@ -51,18 +49,18 @@ printf("In ReadMatrix\n");
 		info->rows++;
 	}
 	free(read_line);
-printf("finish matrixinfo\n");
+
 	/* Allocating value to array */
-	info->matrix_size = info->rows*info->columns;
-	int *mat = (int*)malloc(sizeof(int)*info->matrix_size);
-printf("matrix_size: %d\n", info->matrix_size);
-printf("finish malloc mat\n");
+	int **mat = (int**)malloc(sizeof(int*)*info->rows);
+	for(int i = 0; i < info->rows; i++){
+		mat[i] = (int*)malloc(sizeof(int)*info->columns);
+	}
 	fseek(fp, 0, SEEK_SET);
 	int temp;
 	for(int i = 0; i < info->rows; i++){
 		for(int j = 0; j < info->columns; j++){
 			fscanf(fp, "%d", &temp);
-			mat[i*info->rows + j] = temp;
+			mat[i][j] = temp;
 		}
 	}
 	
@@ -74,9 +72,9 @@ int ModifyGCD(int rows, int columns){
 printf("in ModifyGCD\n");
 	int ret = 0, temp;
 	for(int i = 1; i<= rows && i<= columns; i++){
-		if(((rows%i == 0) && (columns%i == 0)) == 0){
+		if((rows%i == 0) && (columns%i == 0)){
 			temp = i;
-			if(abs(TILE_SIZE-temp) <= abs(TILE_SIZE-ret)){
+			if(abs(STANDARD_SIZE-temp) <= abs(STANDARD_SIZE-ret)){
 				ret = temp;
 			}
 			else	break;
@@ -85,11 +83,11 @@ printf("in ModifyGCD\n");
 
 	return ret;
 }
-/*
-void *ThreadFunc(void *arg){
 
+void *ThreadFunc(void *arg){
+	Parameter thr_arg = (*Parameter)arg;
 }
-*/
+
 int main(int argc, char **argv){
 	/* Checking Excution Format */
 	if(argc != 3){
@@ -99,8 +97,8 @@ int main(int argc, char **argv){
 
 	/* Setting Input Matrix */
 	MatrixInfo A_Info, B_Info;
-	int *A_Mat = ReadMatrix(argv[1], &A_Info);
-	int *B_Mat = ReadMatrix(argv[2], &B_Info);
+	int **A_Mat = ReadMatrix(argv[1], &A_Info);
+	int **B_Mat = ReadMatrix(argv[2], &B_Info);
 
 	/* Check operation is possible */
 	if(A_Info.columns != B_Info.rows){
@@ -112,35 +110,49 @@ int main(int argc, char **argv){
 	MatrixInfo res_Info;
 	res_Info.rows= A_Info.rows;
 	res_Info.columns = B_Info.columns;
-	int *res_Mat = (int*)malloc(sizeof(int)*(res_Info.rows*res_Info.columns));
-printf("finish setting result_mat\n");
-	/* Setting clock time 
+	int **res_Mat = (int**)malloc(sizeof(int*)*res_Info.rows);
+	for(int i = 0; i < res_Info.rows; i++){
+		res_Mat[i] = (int*)malloc(sizeof(int)*res_Info.columns);
+	}
+	
+	/* Setting clock time */
 	struct timespec start, end;
 	double excution_time = 0;
-*/
-	/* Make multi-thread 
-	int tile_row, tile_col;
-	tile_row = res_Info.rows/TILE_SIZE;
-	tile_col = res_Info.columns/TILE_SIZE;
+
+	/* Preparation process to divide Matrix into Tiles */
+	int tile_row, tile_col, tile_size;
+	tile_size = ModifyGCD(res_Info.rows, res_Info.columns);
+	tile_row = res_Info.rows/tile_size;
+	tile_col = res_Info.columns/tile_size;
+
+	/* Setting number of tiles for each thread */
+	int Ntile = tile_row*tile_col;
+	int alloc_tile[Nthread];
+	if((Ntile % Nthread) == 0){
+		for(int i = 0; i < Nthread; i++){
+			alloc_tile[i] = Ntile/Nthread;
+		}
+	}
+	else{
+		int rest = Ntile;
+		for(int i = 0; i < Nthread-1; i++){
+			alloc_tile[i] = Ntile/Nthread;
+			rest -= alloc_tile[i];
+		}
+		alloc_tile[Nthread-1] = rest;
+	}
+
+	/* Create multi-thread */
 	pthread_t *multi_thread = (pthread_t*)malloc(sizeof(pthread_t)*Nthread);
 	Parameter *thr_par;
-*/
-	/* Setting number of tiles for each thread */
-
-
-
-int a = ModifyGCD(res_Info.rows, res_Info.columns);
-printf("ModifyGCD: %d\n", a);
-
-/*
 	clock_gettime(CLOCK_MONOTONIC, &start);
 	for(int i = 0; i < Nthread; i++){
 		int rc;
+		thr_par.tid = i;
+		thr_par.num_tile = alloc_tile[i];
 		thr_par.A_Mat = A_Mat;
 		thr_par.B_Mat = B_Mat;
 		thr_par.res_Mat = res_Mat;
-//		thr_par.A_Info = A_Info;
-//		thr_par.B_Info = B_Info;
 		thr_par.res_Info = res_Info;
 
 		if((rc = pthread_create(&(multi_thread), NULL, 
@@ -148,7 +160,19 @@ printf("ModifyGCD: %d\n", a);
 			perror("Multi Thread Create Error!");
 		}
 	}
-*/
+
+	/* Join multi-thread */
+	void *thr_ret;
+	for(int i = 0; i < Nthread; i++){
+		int rc;
+		if((rc = pthread_join(multi_thread[i], &thr_ret)) != 0){
+			perror("Multi Thread Join Failed!");
+		}
+	}
+	clock_gettime(CLOCK_MONOTONIC, &end);
+	excution_time = end.tv_sec - start.tv_sec;
+	printf("Multi-Threading Excution Time: %f\n", excution_time);
+
 	/* Write result-matrix to file */
 	FILE *res_file = fopen("Result", "w");
 	if(res_file == NULL){
@@ -157,16 +181,26 @@ printf("ModifyGCD: %d\n", a);
 	}
 	for(int i = 0; i < res_Info.rows; i++){
 		for(int j = 0; j < res_Info.columns; j++){
-			fprintf(res_file, " %d ", res_Mat[i*res_Info.rows + j]);
+			fprintf(res_file, " %d ", res_Mat[i][j]);
 		}
 		fprintf(res_file, "\n");
 	}
 	fclose(res_file);
 
 	/* free allocated memory */
+	for(int i = 0; i < A_Info.rows; i++){
+		free(A_Mat[i]);
+	}
 	free(A_Mat);
+	for(int i = 0; i < B_Info.rows; i++){
+		free(B_Mat[i]);
+	}
 	free(B_Mat);
+	for(int i = 0; i < res_Info.rows; i++){
+		free(res_Mat[i]);
+	}
 	free(res_Mat);
+	free(multi_thread);
 
 	return 0;
 }
